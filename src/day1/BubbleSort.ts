a7712658f3a18e3006069a9412baab9f75543e5d
ahmdsabbir <ahmd.sabbir@gmail.com>
2024-07-28 15:00:10 +0600

    bubble sort alt approach

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,10 +1,22 @@
+// export default function bubbleSort(arr: number[]): void {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length - 1 - i; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 const temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+// }
+
 export default function bubbleSort(arr: number[]): void {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                const temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+    for (let i=0; i < arr.length; i++) {
+        for (let j=i+1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                const temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
             }
         }
     }
